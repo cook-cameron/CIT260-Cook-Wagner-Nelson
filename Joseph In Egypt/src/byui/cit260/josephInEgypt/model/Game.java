cf1a80812e483234adde2db6caa1020ac49c3d84
cook-cameron <32019675+cook-cameron@users.noreply.github.com>
2017-10-04 01:06:27 -0600

    10/3/17 Initial Commit Repository 1

---
+++ Joseph In Egypt/src/byui/cit260/josephInEgypt/model/Game.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.josephInEgypt.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author cameroncook
+ */
+public class Game implements Serializable {
+    
+    
+    private Player Player;
+    
+    
+
+    public Player getPlayer() {
+        return Player;
+    }
+
+    public void setPlayer(Player player) {
+        this.Player = player;
+    }
+    
+    
+}
---
+++ Joseph In Egypt/src/byui/cit260/josephInEgypt/model/Player.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.josephInEgypt.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author cameroncook
+ */
+public class Player implements Serializable{
+    
+    //calss instance variables
+    private String name;
+    private double bestTime;
+    private ArrayList<Game> games = new ArrayList<Game>();
+    
+
+    public Player() {
+    }
+
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
+
+   
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
