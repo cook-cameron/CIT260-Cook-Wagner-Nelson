afd7eabe615fcb9f6f4f0d43f6359401223f5bbf
cook-cameron <32019675+cook-cameron@users.noreply.github.com>
2017-11-12 04:23:34 -0700

    Updated view Layer-- Approved CC

--- Joseph In Egypt/test/josephInEgypt/control/CropsControlTest.java
+++ Joseph In Egypt/test/josephInEgypt/control/CropsControlTest.java
@@ -5,92 +5,93 @@
  */
 package josephInEgypt.control;
 
+import josephInEgypt.control.CropsControl;
 import byui.cit260.josephInEgypt.model.Crops;
-import static org.junit.Assert.*;
+import static josephInEgypt.control.CropsControl.calcLandCost;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author cameroncook
  */
 public class CropsControlTest {
-
-
-    /**
-     *
-     */
-public void BuyLand()
-{
-    System.out.println("testBuyLand");
-System.out.println("** buyLand test case 1 **");
-// we need a crops object with the following values filled in
-Crops theCrops = new Crops();
-theCrops.setWheatInStore(2800);
-theCrops.setAcres(0);
-int toBuy = 10;
-int landCost = 20;
-CropsControl instance = new CropsControl();
-int expResult = 800;
-int result = instance.buyLand(theCrops, toBuy, landCost);
-assertEquals(expResult, result);
-                
-        
-        
+    
+    public CropsControlTest() {
     }
 
+    /**
+     * Test of calcLandCost method, of class CropsControl.
+     */
+    @Test
+    public void testCalcLandCost() {
+        System.out.println("calcLandCost");
+        System.out.println("calcLandCost");
+        int expResult = 0;
+        int result = CropsControl.calcLandCost();
+        
+        if ((result >= 17) && (result <= 27))
+            System.out.println("sucess");
+        
+        else 
+            System.out.println("error");
+    }
 
     /**
      * Test of buyLand method, of class CropsControl.
      */
     @Test
-    public void testBuyLand() 
-    {
+    public void testBuyLand() {
         System.out.println("buyLand");
-        Crops theCrops = null;
-        int ToBuy = 0;
-        int landCost = 0;
-        int expResult = 0;
-        int result = CropsControl.buyLand(theCrops, ToBuy, landCost);
+        Crops theCrops = new Crops();
+        theCrops.setWheatInStore(2800);
+        theCrops.setAcres(100);
+        
+        int toBuy = 10;
+        int landPrice = 20;
+        int expResult = 2600;
+        int result = CropsControl.buyLand(theCrops, toBuy, landPrice);
         assertEquals(expResult, result);
 
     }
-    
-    
-    
-    
-    
-    
-            public void plantCrops()
-        {
-                System.out.println("testPlantCrops");
-System.out.println("** plantCrops test case 1 **");
-            Crops theCrops = new Crops();
-theCrops.setWheatInStore(2800);
-theCrops.setAcres(100);
-int toPlant = 10;
-int pricePerAcre = 2;
-CropsControl instance = new CropsControl();
-int expResult = 0;
-int result = instance.plantCrops(theCrops, toPlant, pricePerAcre);
-assertEquals(expResult, result);
-            
-            
-            
-        }
-            
-            @Test
-            
-            public void testPlantCrops() 
-            {
-        System.out.println("plantCrops");
-        Crops theCrops = null;
-        int toPlant = 0;
-        int pricePerAcre = 0;
-        int expResult = 0;
-        int result = CropsControl.plantCrops(theCrops, toPlant, pricePerAcre);
-        assertEquals(expResult, result);
 
-                
-                
-}
+    /**
+     * Test of sellLand method, of class CropsControl.
+     */
+    @Test
+    public void testSellLand() {
+        System.out.println("sellLand");
+        Crops theCrops = new Crops();
+        theCrops.setAcres(100);
+        theCrops.setWheatInStore(2800);
+        
+        
+        int toSell = 10;
+        CropsControl instance = new CropsControl();
+        int expResult = 90;
+        int result = instance.sellLand(theCrops, toSell);
+        assertEquals(expResult, result);
+        System.out.println(calcLandCost());
+    }
+
+    /**
+     * Test of plantCrops method, of class CropsControl.
+     */
+    @Test
+    public void testPlantCrops() {
+        System.out.println("plantCrops");
+        Crops theCrops = new Crops();
+        theCrops.setWheatInStore(2800);
+        theCrops.setAcres(100);
+        
+        
+        int toPlant = 100;
+        CropsControl instance = new CropsControl();
+        int expResult = 2600;
+        int result = instance.plantCrops(theCrops, toPlant);
+        assertEquals(expResult, result);
+        System.out.print(theCrops);
+      
+    }
+    
 }
