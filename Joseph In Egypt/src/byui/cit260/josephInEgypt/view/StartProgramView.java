971439627407fba516738718e3a2db3b31c0449d
cook-cameron <32019675+cook-cameron@users.noreply.github.com>
2017-11-12 03:42:58 -0700

    Updated view Layer-- Approved CC

--- Joseph In Egypt/src/byui/cit260/josephInEgypt/view/StartProgramView.java
+++ Joseph In Egypt/src/byui/cit260/josephInEgypt/view/StartProgramView.java
@@ -32,19 +32,35 @@ public final class StartProgramView {
     public void displayBanner() {
         
              System.out.println(
-        "\n****************************************************************"
-      + "\n*                                                              *"
-      + "\n* Welcome to the game of Joseph in Egypt. In this game, you    *"
-      + "\n* will assume the role of an overseer for your village. Tread  *"
-      + "\n* carefully, if your people are not fed well, you may lose     *"
-      + "\n* your head.                                                   *"
-      + "\n                                                               *"
-      + "\n****************************************************************"   
+          "\n**************************************************************"
+        + "\n* Welcome to Pharaoh’s court. You have been summoned         *" 
+        + "\n* here to assume your new role as Agricultural Overseer.     *"
+        + "\n* You report directly to Joseph, Pharaoh’s Vizier. Your      *"
+        + "\n* responsibility is to gather the grain from the harvest,    *"
+        + "\n* provide enough food to the people so they do not go hungry,*"
+        + "\n* and reserve the rest in Pharaoh’s granary in preparation   *"
+        + "\n* for the prophesied famine that is to come in seven years.  *" 
+        + "\n* You need to know how to plant crops, develop land, feed the*"
+        + "\n*people and store the excess. If you fail to provide enough  *"
+        + "\n* food for the people, the people will starve. Some people   *"
+        + "\n* will die. Your workforce will be diminished, and you will  *"
+        + "\n* not have enough workers to plant and harvest crops. Pharaoh*"
+        + "\n* will not be pleased with those who fail him. Those who fail*"
+        + "\n* are thrown in prison. He's not very nice to prisoners--just*"
+        + "\n* ask the Royal Baker. Oh, you can't because he's not around *"
+        + "\n* anymore,RIP.                                               *"
+        + "\n*            Be wise and you will be successful.             *"
+        + "\n**************************************************************"   
        );
     }
 
+    /**
+     *
+     */
     public void displayStartProgramView() {
-        System.out.println("\n*** displayStartProgram() function called ***");
+       System.out.println("\n*** displayStartProgram() function called ***"); //To change body of generated methods, choose Tools | Templates.
     }
+
+
     
 }
